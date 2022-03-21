84433682fa460f61945da6b34e4a2492abd599ca
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-03-21 15:19:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet().
+def greet():
+# Write 3 print statements inside the function.
+  greeting = input('Please enter your name?\n').capitalize()
+  print(f"Hi! {greeting}.")
+  print(f'{greeting} is a very nice name.')
+  print(f'Today we will be learning about function {greeting}.' )
+# Call the greet() function and run your code.
+greet()
+
