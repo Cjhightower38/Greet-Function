1b18e20e359a63153cad49b837da30f81695fd23
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-03-21 15:38:50 +0000

    add second parameter to original code

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 # Review: 
 # Create a function called greet().
 # Response to the question.
-# def greet():
+def greet(greeting, location):
 # # Write 3 print statements inside the function.
-#   greeting = input('Please enter your name?\n').capitalize()
-#   print(f"Hi! {greeting}.")
-#   print(f'{greeting} is a very nice name.')
-#   print(f'Today we will be learning about function {greeting}.' )
+  greeting = input('Please enter your name?\n').capitalize()
+  location = input('What state do you live in?\n').capitalize()
+  print(f"Hi! {greeting}.")
+  print(f'{greeting} is a very nice name.')
+  print(f'Today we will be learning about function {greeting}.')
+  print(f"How's the weather in {location}?")
 # # Call the greet() function and run your code.
-# greet()
+greet('greeting', 'location')
 
-# This way is a little less interactive.
-def greet_with_parameter(name):
-# Write 3 print statements inside the function.
-  print(f"Hi! {name}.")
-  print(f'{name} is a very nice name.')
-  print(f'Today we will be learning about function {name}.' )
-# Call the greet() function and run your code.
-# A name has to be entered or a error will be thrown.
-greet_with_parameter("Jack")
+# # This way is a little less interactive.
+# def greet_with_parameter(name):
+# # Write 3 print statements inside the function.
+#   print(f"Hi! {name}.")
+#   print(f'{name} is a very nice name.')
+#   print(f'Today we will be learning about function {name}.' )
+# # Call the greet() function and run your code.
+# # A name has to be entered or a error will be thrown.
+# greet_with_parameter("Jack")
