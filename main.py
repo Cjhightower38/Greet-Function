1f7d00d4ac4e9091f0353d80015fa5dd053a2540
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-03-21 15:52:05 +0000

    add keyword arguments

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-# Review: 
-# Create a function called greet().
-# Response to the question.
-def greet(greeting, location):
-# # Write 3 print statements inside the function.
-  greeting = input('Please enter your name?\n').capitalize()
-  location = input('What state do you live in?\n').capitalize()
-  print(f"Hi! {greeting}.")
-  print(f'{greeting} is a very nice name.')
-  print(f'Today we will be learning about function {greeting}.')
-  print(f"How's the weather in {location}?")
-# # Call the greet() function and run your code.
-greet('greeting', 'location')
+# # Review: 
+# # Create a function called greet().
+# # Response to the question.
+# def greet(greeting, location):
+# # # Write 3 print statements inside the function.
+#   greeting = input('Please enter your name?\n').capitalize()
+#   location = input('What state do you live in?\n').capitalize()
+#   print(f"Hi! {greeting}.")
+#   print(f'{greeting} is a very nice name.')
+#   print(f'Today we will be learning about function {greeting}.')
+#   print(f"How's the weather in {location}?")
+# # # Call the greet() function and run your code.
+# greet('greeting', 'location')
 
-# # This way is a little less interactive.
-# def greet_with_parameter(name):
-# # Write 3 print statements inside the function.
-#   print(f"Hi! {name}.")
-#   print(f'{name} is a very nice name.')
-#   print(f'Today we will be learning about function {name}.' )
-# # Call the greet() function and run your code.
-# # A name has to be entered or a error will be thrown.
-# greet_with_parameter("Jack")
+
+
+# This way is a little less interactive.
+def greet_with_parameter(name, location):
+# Write 3 print statements inside the function.
+  print(f"Hi! {name}.")
+  print(f'{name} is a very nice name.')
+  print(f'Today we will be learning about function {name}.' )
+  print(f"How's the weather in {location}?")
+# Call the greet() function and run your code.
+# A name has to be entered or a error will be thrown.
+# Using keyword arguments.
+greet_with_parameter(name = 'Jack', location = 'Florida')
